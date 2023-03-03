a074d3e0aff86aef06f0602f13420b1bfbf43ea6
ynsun <ynsun@thoughtworks.com>
2023-03-03 16:14:34 +0800

    import element ui

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-menu
+    :default-active="activeIndex"
+    class="el-menu-demo"
+    mode="horizontal"
+    :ellipsis="false"
+    @select="handleSelect"
+  >
+    <el-menu-item index="0">Yanan Sun</el-menu-item>
+    <div class="flex-grow" />
+    <el-menu-item index="1">Home</el-menu-item>
+    <el-menu-item index="2">Blogs</el-menu-item>
+    <el-menu-item index="3">Contact Me</el-menu-item>
+  </el-menu>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const activeIndex = ref('1')
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath)
+}
+</script>
+
+<style>
+.flex-grow {
+  flex-grow: 1;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <el-header><NavigationBar></NavigationBar></el-header>
+  <el-main>
+    <el-row class="ep-bg-purple">
+      <el-col :span="12">
+        <div class="grid-content box-retro-content">
+          <h1 class="title">
+            I'm Yanan Sun. <br />
+            UI Developer
+          </h1>
+          <p class="introduction">
+            I am a web developer focused on crafting great web experiences. I enjoy creating
+            beautifully designed, intuitive and functional websites for my clients.
+          </p>
+        </div>
+      </el-col>
+      <el-col :span="12"><div class="grid-content photo" /></el-col>
+    </el-row>
+  </el-main>
+  <section class="section"></section>
+</template>
+
+<script lang="ts" setup>
+import NavigationBar from '@/components/NavigationBar.vue'
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+.box-retro-content {
+  padding-top: 210px;
+  padding-bottom: 210px;
+}
+</style>
